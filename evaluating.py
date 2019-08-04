b4ef485b9c2b8c7cc2389d2d9272d361b00064c6
mosi <momkdh2@naver.com>
2019-08-04 10:37:09 +0900

    operate well

---
+++ evaluating.py
@@ -0,0 +1,57 @@
+from modeling import BayesianNetwork
+
+import os
+import argparse
+import numpy as np
+
+import torch
+import torch.utils.data as data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+
+def test_ensemble(model, args):
+    model.eval()
+    correct = 0
+    corrects = np.zeros(args.test_sample+1, dtype=int)
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(args.device), target.to(args.device)
+            outputs = torch.zeros(args.test_sample+1, data.shape[0], args.classes).to(args.device)
+            for i in range(args.test_sample):
+                outputs[i] = model(data, sample=True)
+            outputs[args.test_sample] = model(data, sample=False)
+            output = outputs.mean(0)
+            preds = outputs.max(2, keepdim=True)[1]
+            pred = output.max(1, keepdim=True)[1] # index of max log-probability
+            corrects += preds.eq(target.view_as(pred)).sum(dim=1).squeeze().cpu().numpy()
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    for index, num in enumerate(corrects):
+        if index < args.test_sample:
+            print('Component {} Accuracy: {}/{}'.format(index, num, TEST_SIZE))
+        else:
+            print('Posterior Mean Accuracy: {}/{}'.format(num, TEST_SIZE))
+    print('Ensemble Accuracy: {}/{}'.format(correct, TEST_SIZE))
+
+if __name__ == "__main__":
+    # hyper parameter
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--sigma_1", type=int, default=0)
+    parser.add_argument("--sigma_2", type=int, default=-6)
+    parser.add_argument("--pi", type=int, default=0.5)
+    parser.add_argument("--samples", type=int, default=2)
+    parser.add_argument("--test_sample", type=int, default=10)
+    parser.add_argument("--classes", type=int, default=10)
+    parser.add_argument("--batch_size", type=int, default=256)
+    args = parser.parse_args()
+    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # download data
+    transform = transforms.Compose([transforms.ToTensor()])
+    test_mnist_data = dset.MNIST('./raw_data/', train=False, download=True, transform=transform)
+    test_loader = data.DataLoader(test_mnist_data, batch_size=args.batch_size)
+    TEST_SIZE = len(test_loader.dataset)
+
+    model = BayesianNetwork(args).to(args.device)
+    model.load_state_dict(torch.load(os.path.join(os.getcwd(), "saved_model/w_u")))
+    test_ensemble(model, args)
--- modeling.py
+++ modeling.py
@@ -11,18 +11,20 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 
 class Gaussian(object):
-    def __init__(self, mu, rho):
+    def __init__(self, mu, rho, args):
         super(Gaussian, self).__init__()
+        self.args = args
         self.mu = mu
         self.rho = rho
         self.normal = torch.distributions.normal.Normal(0, 1)
+        # self.sigma = torch.log(1+torch.exp(self.rho)).to(device)
 
     @property
     def sigma(self):
         return torch.log(1+torch.exp(self.rho))
 
     def sample(self):
-        epsilon = self.normal.sample(self.rho.size()).to(device)
+        epsilon = self.normal.sample(self.rho.size()).to(self.args.device)
         return self.mu + self.sigma * epsilon
 
     def log_prob(self, input):
@@ -30,12 +32,12 @@ class Gaussian(object):
                 -torch.log(self.sigma)
                 -((input-self.mu)**2) / (2 * (self.sigma**2))).sum()
 
-class ScaleMixtureGaussian():
-    def __init__(self, pi, sigma1, sigma2):
+class ScaleMixtureGaussian(object):
+    def __init__(self, pi, sigma1, sigma2, args):
         super(ScaleMixtureGaussian, self).__init__()
         self.pi = pi
-        self.sigma1 = sigma1
-        self.sigma2 = sigma2
+        self.sigma1 = torch.FloatTensor([math.exp(sigma1)]).to(args.device)
+        self.sigma2 = torch.FloatTensor([math.exp(sigma2)]).to(args.device)
         self.gaussian1 = torch.distributions.normal.Normal(0, self.sigma1)
         self.gaussian2 = torch.distributions.normal.Normal(0, self.sigma2)
 
@@ -45,26 +47,27 @@ class ScaleMixtureGaussian():
         return (torch.log(self.pi * prob1 + (1 - self.pi) * prob2)).sum()
 
 class BayesianLinear(nn.Module):
-    def __init__(self, in_dim, out_dim):
+    def __init__(self, in_dim, out_dim, args):
         super(BayesianLinear, self).__init__()
+        self.args = args
         self.in_dim = in_dim
         self.out_dim = out_dim
         # weight parameters
         self.weight_mu = nn.Parameter(torch.Tensor(self.out_dim, self.in_dim).uniform_(-0.1, 0.1))
         self.weight_rho = nn.Parameter(torch.Tensor(self.out_dim, self.in_dim).uniform_(-0.1, 0.1))
-        self.weight = Gaussian(self.weight_mu, self.weight_rho)
+        self.weight = Gaussian(self.weight_mu, self.weight_rho, args)
         # bias parameters
-        self.bias_mu = nn.Parameter(torch.Tensor(self.out_dim, self.in_dim).uniform_(-0.1, 0.1))
-        self.bias_rho = nn.Parameter(torch.Tensor(self.out_dim, self.in_dim).uniform_(-0.1, 0.1))
-        self.bias = Gaussian(self.bias_mu, self.bias_rho)
+        self.bias_mu = nn.Parameter(torch.Tensor(self.out_dim).uniform_(-0.1, 0.1))
+        self.bias_rho = nn.Parameter(torch.Tensor(self.out_dim).uniform_(-0.1, 0.1))
+        self.bias = Gaussian(self.bias_mu, self.bias_rho, args)
         # prior distributions
-        self.weight_prior = ScaleMixtureGaussian(PI, SIGMA_1, SIGMA_2)
-        self.bias_prior = ScaleMixtureGaussian(PI, SIGMA_1, SIGMA_2)
+        self.weight_prior = ScaleMixtureGaussian(args.pi, args.sigma_1, args.sigma_2, args)
+        self.bias_prior = ScaleMixtureGaussian(args.pi, args.sigma_1, args.sigma_2, args)
         self.log_prior = 0
         self.log_variational_posterior = 0
 
-    def forward(self, input):
-        if self.training:
+    def forward(self, input, sample):
+        if self.training or sample:
             weight = self.weight.sample()
             bias = self.bias.sample()
             self.log_prior = self.weight_prior.log_prob(weight) + self.bias_prior.log_prob(bias) # log P(w)
@@ -76,50 +79,65 @@ class BayesianLinear(nn.Module):
             self.log_variational_posterior = 0
         return f.linear(input, weight, bias)
 
-
 class BayesianNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, args):
         super(BayesianNetwork, self).__init__()
-        self.linear1 = BayesianLinear(784, 400)
-        self.linear2 = BayesianLinear(400, 400)
-        self.linear3 = BayesianLinear(400, 10)
+        self.args = args
+        self.linear1 = BayesianLinear(784, 400, self.args)
+        self.linear2 = BayesianLinear(400, 400, self.args)
+        self.linear3 = BayesianLinear(400, 10, self.args)
 
-    def forward(self, x):
+    def forward(self, x, sample=False):
         x = x.view(-1, 28 * 28)
-        x = f.relu(self.l1(x))
-        x = f.relu(self.l2(x))
-        x = f.log_softmax(self.l3(x), dim=1)
+        x = f.relu(self.linear1(x, sample))
+        x = f.relu(self.linear2(x, sample))
+        x = f.log_softmax(self.linear3(x, sample), dim=1)
         return x
 
-    def sample_elbo(self):
-        outputs = torch.zeros(samples, BATCH_SIZE, CLASSES).to(device)
+    def log_prior(self):
+        return self.linear1.log_prior + self.linear2.log_prior + self.linear3.log_prior
 
-        return loss
+    def log_variational_posterior(self):
+        return self.linear1.log_variational_posterior + self.linear2.log_variational_posterior + self.linear3.log_variational_posterior
 
-def train(model, optimizer, epoch):
+    def sample_elbo(self, data, target):
+        outputs = torch.zeros(args.samples, data.shape[0], args.classes).to(self.args.device)
+        log_priors = torch.zeros(args.samples).to(self.args.device)
+        log_variational_posteriors = torch.zeros(args.samples).to(self.args.device)
+        for i in range(args.samples):
+            outputs[i] = self.forward(data)
+            log_priors[i] = self.log_prior()
+            log_variational_posteriors[i] = self.log_variational_posterior()
+        log_prior = log_priors.mean()
+        log_variational_posterior = log_variational_posteriors.mean()
+        negative_log_likelihood = f.nll_loss(outputs.mean(0), target, size_average=False).to(self.args.device)
+        loss = (log_variational_posterior - log_prior) / NUM_BATCHES + negative_log_likelihood
+        return loss#, log_prior, log_variational_posterior, negative_log_likelihood
+
+def train(model, optimizer, epoch, args):
     model.train()
+    loss_list = []
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+        data, target = data.to(args.device), target.to(args.device)
         model.zero_grad()
         loss = model.sample_elbo(data, target)
+        loss_list.append(loss.item())
         loss.backward()
         optimizer.step()
-
-    return
-
-
+    print("epoch : {}, loss : {}".format(epoch, sum(loss_list)/len(loss_list)))
 
 if __name__ == "__main__":
     # hyper parameter
     parser = argparse.ArgumentParser()
-    parser.add_argument("--epoch", type=int, default=10)
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--sigma_1", type=int, default=0)
+    parser.add_argument("--sigma_2", type=int, default=-6)
+    parser.add_argument("--pi", type=int, default=0.5)
+    parser.add_argument("--samples", type=int, default=2)
+    parser.add_argument("--classes", type=int, default=10)
+    parser.add_argument("--batch_size", type=int, default=256)
     args = parser.parse_args()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    PI = 0.5
-    SIGMA_1 = torch.cuda.FloatTensor([math.exp(-0)])
-    SIGMA_2 = torch.cuda.FloatTensor([math.exp(-6)])
-    CLASSES = 10
-    BATCH_SIZE = 100
+    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # folder for download
     try:
@@ -130,20 +148,21 @@ if __name__ == "__main__":
     # download data
     transform = transforms.Compose([transforms.ToTensor()])
     train_mnist_data = dset.MNIST('./raw_data/', train=True, download=True, transform=transform)
-    test_mnist_data = dset.MNIST('./raw_data/', train=False, download=True, transform=transform)
-    train_loader = data.DataLoader(train_mnist_data)
-    test_loader = data.DataLoader(test_mnist_data)
+    train_loader = data.DataLoader(train_mnist_data, batch_size=args.batch_size)
+    NUM_BATCHES = len(train_loader)
 
     # modeling
-    model = BayesianNetwork().to(device)
+    model = BayesianNetwork(args).to(args.device)
     optimizer = optim.Adam(model.parameters())
 
     # training
-    for epoch in range(args.epoch):
-        train(model, optimizer, epoch)
+    for epoch in range(args.epochs):
+        train(model, optimizer, epoch, args)
 
-    # folder for save model
+    # save model
     try:
         os.makedirs("saved_model")
     except FileExistsError as e:
         pass
+    torch.save(model.state_dict(), os.path.join(os.getcwd(), "saved_model/w_u"))
+
